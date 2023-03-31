4d7fd4deca14eb1e1fb8744a1110f3b2fe720039
Jurandi Junior <jurandijr20@gmail.com>
2023-03-31 20:43:53 -0300

    Escalabilidade de conexões

--- bytebank/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ bytebank/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -4,14 +4,27 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 public class ConnectionFactory {
 
 	public Connection recuperarConexao() {
 		try {
-			return DriverManager.getConnection("jdbc:mysql://localhost:3306/byte_bank?user=junior&password=@Testando");
+			return createDataSource().getConnection();
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 	}
+	
+    private HikariDataSource createDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank");
+        config.setUsername("junior");
+        config.setPassword("@Testando");
+        config.setMaximumPoolSize(10);
+
+        return new HikariDataSource(config);
+    }
 
 }
